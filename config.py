46071568e6a877d4a25c8add2ccd17282d91a115
Your Name <you@example.com>
2018-07-14 12:29:10 -0400

    removing config.py

--- config.py
+++
@@ -1,5 +0,0 @@
-# Twitter API Keys
-consumer_key = "Ed4RNulN1lp7AbOooHa9STCoU"
-consumer_secret = "P7cUJlmJZq0VaCY0Jg7COliwQqzK0qYEyUF9Y0idx4ujb3ZlW5"
-access_token = "839621358724198402-dzdOsx2WWHrSuBwyNUiqSEnTivHozAZ"
-access_token_secret = "dCZ80uNRbFDjxdU2EckmNiSckdoATach6Q8zb7YYYE5ER"
